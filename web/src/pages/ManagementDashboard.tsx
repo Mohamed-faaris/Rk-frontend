b4d355dfc6b009109ac4bb50bd61c7501ab1f8ad
Sivasuriyan Raja <sivasuriyanraja569@gmail.com>
2026-08-04 16:14:00 +0530

    Abu photo and ManageDashboard

--- web/src/pages/ManagementDashboard.tsx
+++ web/src/pages/ManagementDashboard.tsx
@@ -1447,11 +1447,11 @@ export default function ManagementDashboard() {
             </TabsTrigger>
             <TabsTrigger value="employees" className="text-xs md:text-sm text-[#D4AF37] data-[state=active]:bg-[#D4AF37] data-[state=active]:text-black py-2 md:py-3 px-1 md:px-4">
               <Users className="w-3 h-3 md:w-4 md:h-4 mr-0 md:mr-2" />
-              <span className="hidden md:inline">Employees</span>
+              <span className="hidden md:inline">Interns</span>
             </TabsTrigger>
             <TabsTrigger value="applications" className="text-xs md:text-sm text-[#D4AF37] data-[state=active]:bg-[#D4AF37] data-[state=active]:text-black py-2 md:py-3 px-1 md:px-4">
               <FileText className="w-3 h-3 md:w-4 md:h-4 mr-0 md:mr-2" />
-              <span className="hidden md:inline">Apps</span>
+              <span className="hidden md:inline">Applications</span>
             </TabsTrigger>
             <TabsTrigger value="projects" className="text-xs md:text-sm text-[#D4AF37] data-[state=active]:bg-[#D4AF37] data-[state=active]:text-black py-2 md:py-3 px-1 md:px-4">
               <FolderKanban className="w-3 h-3 md:w-4 md:h-4 mr-0 md:mr-2" />
